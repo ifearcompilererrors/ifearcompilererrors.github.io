42c8d4ea7f9e8ab2e914f65da1b3ef1d8ed7e95c
Angelique De Castro <angeliqueajdecastro@gmail.com>
2015-03-23 19:57:04 -0700

    refactor for django

---
+++ ifearcompilererrors/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from ifearcompilererrors import settings
+from home import views
+from settings import MEDIA_ROOT, STATIC_ROOT
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ifearcompilererrors.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
