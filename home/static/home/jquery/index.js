38f9102f6dc753a1cb4bcce25b2df95fe8a0e700
Angelique De Castro <angeliqueajdecastro@gmail.com>
2015-03-29 21:00:57 -0700

    replace banner with stars

--- home/static/home/jquery/index.js
+++ home/static/home/jquery/index.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+var k = Math.random()*1000; // number of stars
+var j = 100; // justify position
+
+for(var i = 0; i < k; ++i){
+    $("#sky").append("<div class='star' style='position:absolute;top:"+(Math.random()*j)+";left:"+(Math.random()*j-150)+";'""></div>");
+}
+
+function twinkle(){
+    var $star = $(".star");
+    $star.delay((Math.random()*100)+1200);
+    $star.animate({opacity:'+=1'}, 100);
+    $star.animate({opacity:'=1'}, 100);
+    $star.animate({opacity:'-=0.5'}, 100, twinkle);
+}
+
+twinkle();
+
 $('.anchorLink').click(function(){
 	$('html, body').animate({
 		scrollTop: "800px"
@@ -20,24 +37,23 @@ $('section[data-type="background"]').each(function()
 });
 
 /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.hide-me').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window+1200 > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},1100);
-                    
-            }
-            
-        }); 
-    
-    });
+$(window).scroll( function(){
 
+    /* Check the location of each desired element */
+    $('.hide-me').each( function(i){
+        
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window+1200 > bottom_of_object ){
+            
+            $(this).animate({'opacity':'1'},1100);
+                
+        }
+        
+    }); 
+
+});
 
 });
--- static/jquery/index.js
+++ static/jquery/index.js
@@ -1,43 +1,99 @@
 $(document).ready(function(){
 
+var midStars = 60; /* number of untwinkling stars in middle */
+var twinkleStarsMid = 25; /* number of twinkling stars */
+var mid = 1000; /* justify position */
+
+var leftRightStars = 20; /* number of left and right stars */
+var left = 100; /* justify position */
+var right = 1000; /* justify position */
+
+for(var i = 0; i < midStars; ++i)
+{
+    $("#sky").append("<span class='star' style='position:absolute;top:"+(Math.random()*mid)+"px;left:"+(Math.random()*mid+100)+"px;'></span>");
+}
+
+for(var i = 0; i < leftRightStars; ++i)
+{
+    $("#sky").append("<span class='star' style='position:absolute;top:"+(Math.random()*left*10)+"px;left:"+(Math.random()*left)+"px;'></span>");
+}
+
+for(var i = 0; i < leftRightStars; ++i)
+{
+    $("#sky").append("<span class='star' style='position:absolute;top:"+(Math.random()*right)+"px;left:"+(Math.random()*right+250)+"px;'></span>");
+}
+
+for(var i = 0; i < twinkleStarsMid; ++i)
+{
+    $("#sky").append("<span class='star twinkle1' style='position:absolute;top:"+(Math.random()*mid)+"px;left:"+(Math.random()*mid+100)+"px;'></span>");
+}
+
+for(var i = 0; i < twinkleStarsMid; ++i)
+{
+    $("#sky").append("<span class='star twinkle2' style='position:absolute;top:"+(Math.random()*mid)+"px;left:"+(Math.random()*mid+100)+"px;'></span>");
+}
+
+function twinkle1(){
+    var $star = $(".twinkle1");
+    $star.delay((Math.random()*100)+1200);
+    $star.animate({opacity:'+=1'}, 100);
+    $star.animate({opacity:'=1'}, 100);
+    $star.animate({opacity:'-=0.5'}, 100, twinkle1);
+}
+
+function twinkle2(){
+    var $star = $(".twinkle2");
+    $star.delay((Math.random()*500)+200);
+    $star.animate({opacity:'+=1'}, 100);
+    $star.animate({opacity:'=1'}, 100);
+    $star.animate({opacity:'-=0.5'}, 100, twinkle2);
+}
+
+twinkle1();
+twinkle2();
+
+$(".welcome-text").fadeIn(20000);
+
+/* animate scroll to about section */
 $('.anchorLink').click(function(){
-	$('html, body').animate({
-		scrollTop: "800px"
-	}, 800);
+    $('html, body').animate({
+        scrollTop: "830vh"
+    }, 800);
 });
 
+
+/* contact section motion parallax */
 var $window = $(window);
 
 $('section[data-type="background"]').each(function()
 {
-	var $bgobj = $(this);
-	$(window).scroll(function()
-	{
-		var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-		var coords = '50%' + yPos + 'px';
-		$bgobj.css({ backgroundPosition:coords });
-	});
+    var $bgobj = $(this);
+    $(window).scroll(function()
+    {
+        var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+        var coords = '50%' + yPos + 'px';
+        $bgobj.css({ backgroundPosition:coords });
+    });
 });
 
 /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.hide-me').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window+1200 > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},1100);
-                    
-            }
-            
-        }); 
-    
-    });
+$(window).scroll( function(){
 
+    /* ... check the location of each desired element */
+    $('.hide-me').each( function(i){
+        
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window+1200 > bottom_of_object ){
+            
+            $(this).animate({'opacity':'1'},1100);
+                
+        }
+        
+    }); 
+
+});
 
 });
